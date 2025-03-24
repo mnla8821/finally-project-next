6c5c102ce5ff4d4e0c095329adf08adac4722253
mnla8821 <mnla8821@gmail.com>
2025-03-24 03:40:08 +0300

    upload 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./Component/Header/Header";
-import Footer from "./Sections/Footer/page";
+import Footer from "./Pages/Footer/Footer";
 
 export const metadata: Metadata = {
   title: "المبدع العربي",
